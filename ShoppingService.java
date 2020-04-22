fe839488c31179ba42e656b2907774540403b147
Chiara Lim <47402981+cclimcile@users.noreply.github.com>
2020-04-22 15:24:59 +0800

    Create ShoppingService.java

---
+++ ShoppingService.java
@@ -0,0 +1,13 @@
+public class ShoppingService {
+
+    String service;
+    String csv_file;
+    double delivery_charge;
+
+    public ShoppingService(String service, String csv_file, double delivery_charge){
+        this.service = service;
+        this. csv_file = csv_file;
+        this.delivery_charge = delivery_charge;
+    }
+
+}
